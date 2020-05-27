953bf5c7dce37ec2266bd01cff34142611a6b605
Vinícius Luis da Silva <vinicius.lds.br@gmail.com>
2020-05-26 21:24:16 -0300

    Começo da separação da rotina de busca de históricos e rotina de cadastro de empresas

--- stock_learning_rabbitmq/WebScrappingStub.py
+++ stock_learning_rabbitmq/WebScrappingStub.py
@@ -6,8 +6,11 @@ class WebScrappingStub(Stub):
     def __init__(self, server):
         super().__init__(server, 'stock-learning-web-scrapping')
 
-    def infomoney_ibovespa_initial_load(self, content):
-        self._send('infomoney-ibovespa-initial-load', content)
+    def infomoney_ibovespa_company_data(self):
+        self._send('infomoney-ibovespa-company-data', {})
+
+    def infomoney_ibovespa_historic_data(self, content):
+        self._send('infomoney-ibovespa-historic-data', {})
 
     def infomoney_ibovespa_live_update(self, content):
-        self._send('infomoney-ibovespa-live-update', content)
+        self._send('infomoney-ibovespa-live-update', {})
