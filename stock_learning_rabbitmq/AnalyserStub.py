c5ad13614ad18be2e6a8f6d098fcf9ba71728119
chsponciano <chs.ponciano@gmail.com>
2020-06-07 19:48:25 -0300

    added analyzer handlers

--- stock_learning_rabbitmq/AnalyserStub.py
+++ stock_learning_rabbitmq/AnalyserStub.py
@@ -5,3 +5,12 @@ class AnalyserStub(Stub):
     
     def __init__(self, server):
         super().__init__(server, 'stock-learning-analyser')
+
+    def daily_prediction_startup_handler(self):
+        self._send('daily-prediction-startup-handler', {})
+
+    def daily_prediction_closing_handler(self):
+        self._send('daily-prediction-closing-handler', {})
+
+    def real_time_value_addition_handler(self, content):
+        self._send('real-time-value-addition-handler', {'stocks': content})
